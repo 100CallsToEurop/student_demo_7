4521d8218a4a1655ee6cd99822b8934232849fbb
100CallsToEurop <piaterik123123@mail.ru>
2022-10-21 19:27:25 +0300

    a

--- src/composition-root.ts
+++ src/composition-root.ts
@@ -1,25 +1,20 @@
 import {Container} from "inversify";
 import {UsersService} from "./domian/users.service";
 import {UsersRepository} from "./repositories/users.repository";
-import {UsersController} from "../../game/controllers/user.controller";
-import {AuthController} from "../../game/controllers/auth.controller";
+import {UsersController} from "./controllers/user.controller";
+import {AuthController} from "./controllers/auth.controller";
 import {AuthService} from "./domian/auth.service";
-import {CommentController} from "../../game/controllers/comment.controller";
+import {CommentController} from "./controllers/comment.controller";
 import {CommentService} from "./domian/comments.service";
 import {CommentsRepository} from "./repositories/comments-repository-db";
 import {PostsService} from "./domian/posts.services";
 import {BloggersService} from "./domian/bloggers.service";
 import {BloggersRepository} from "./repositories/bloggers-repository-db";
-import {BloggerController} from "../../game/controllers/blogger.controller";
+import {BloggerController} from "./controllers/blogger.controller";
 import {PostsRepository} from "./repositories/posts-repository-db";
-import {PostsController} from "../../game/controllers/post.controller";
+import {PostsController} from "./controllers/post.controller";
 import {EmailAdapter} from "./adapters/email-adapter";
-import {GameController} from "../../game/game.controller";
-import {GameService} from "../../game/game.service";
-import {GameRepository} from "./repositories/game-repository-db";
-import {QuestionRepository} from "./repositories/question-repository";
-import {QuestionService} from "../../game/question.service.";
-import {QuestionController} from "../../game/question.controller";
+
 
 export const container = new Container()
 //Auth
@@ -43,11 +38,3 @@ container.bind<BloggersService>(BloggersService).to(BloggersService)
 container.bind<BloggersRepository>(BloggersRepository).to(BloggersRepository)
 //Email
 container.bind(EmailAdapter).to(EmailAdapter)
-//Game
-container.bind(GameController).to(GameController)
-container.bind<GameService>(GameService).to(GameService)
-container.bind<GameRepository>(GameRepository).to(GameRepository)
-//Question
-container.bind(QuestionController).to(QuestionController)
-container.bind<QuestionService>(QuestionService).to(QuestionService)
-container.bind<QuestionRepository>(QuestionRepository).to(QuestionRepository)
